a2df53ad5cbb135db87125dc33e01d98b7484da7
Gerard Mennella <gerardmennella21@gmail.com>
2022-05-09 02:01:06 -0400

    delete functionality

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const path = require('path')
 const fs = require('fs')
+const { v4: uuidv4 } = require('uuid')
 
 router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../../db/db.json'))
@@ -12,11 +13,20 @@ router.post('/notes', (req, res) => {
     let newNote = {
         title: req.body.title,
         text: req.body.text,
-        id: db.length
+        id: uuidv4()
     }
     db.push(newNote)
     fs.writeFileSync('db/db.json', JSON.stringify(db))
     res.json(db)
+})
+
+router.delete("/notes/:id", (req, res) => {
+    let db = JSON.parse(fs.readFileSync('db/db.json'))
+
+    let noteDelete = db.filter(item => item.id !== req.params.id)
+
+    fs.writeFileSync('db/db.json', JSON.stringify(noteDelete))
+    res.json(noteDelete)
 })
 
 module.exports = router
